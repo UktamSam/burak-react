dc51a69d77ee18a3c97546f104aa266da8a7dac0
UktamSam <holmatov00@gmail.com>
2026-03-15 16:00:28 +0900

    fix: retrieve fresh menu and modify NewDishes component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,7 +9,7 @@ import "../../../css/home.css"
 
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setnewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
@@ -17,13 +17,14 @@ import { ProductCollection } from "../../../lib/enums/product.enum";
 /* REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({                           //DEFINE
   savePopularDishesToStore: (data: Product[]) => dispatch(setPopularDishes(data)),
-// 1 - (приказ)                // 2 -  (Action из Redux)
+  saveNewDishesToStore: (data: Product[]) => dispatch(setnewDishes(data)),
+// 1 - (команда)                                   // 2 -  (Action из Redux)
 });
 
 
 export default function HomePage() {
   // 2.1: DBdan olingan "result"ni dispatch orqali yuborish
-    const { savePopularDishesToStore } = actionDispatch(useDispatch());             //CALL
+    const { savePopularDishesToStore, saveNewDishesToStore } = actionDispatch(useDispatch());             //CALL
 
   useEffect(() => {
     // 1: BEdan JSON formatda DATA qabul qilamiz (BE DATA FETCH)
@@ -42,6 +43,21 @@ export default function HomePage() {
         console.log("ERROR", err);
         
       });
+
+    product.getProducts({
+      page: 1,
+      limit: 4,
+      order: "createdAt",
+      })
+      .then(data => {
+        console.log("Data passed here:", data);
+        // 2: SLICE: DATA => STORE
+        saveNewDishesToStore(data);
+      }).catch( (err) => {
+        console.log("ERROR", err);
+        
+      });
+
     // @ts-ignore
   }, []);  // 4: INTERACTION
 
